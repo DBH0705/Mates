1d473e786d8c676c4dbe997119b6949199c8365a
DBH0705 <walterdaniel74897209@gmail.com>
2023-07-04 11:30:14 -0500

    wec

---
+++ mate.js
@@ -0,0 +1,32 @@
+
+//alert("Hola Mundooooooo XD jijijiji");
+/*comentario de varias lineas*/
+var codigo;
+let nombre;
+const dato="2023";
+
+edad=10;
+
+if(edad>18){
+    alert("MAYOR DE EDAD");
+}
+else{
+    alert("NOOOO");
+}
+
+
+var contador
+for(contador=0; contador <= 20; contador ++){
+    console.log(contador);
+}
+
+function f_suma(n1,n2){
+    var suma;
+    suma = n1 + n2;
+    return suma;
+}
+
+var resultado;
+ resultado = f_suma(20,5);
+
+ alert("la suma es:" + resultado);
---
+++ ope.js
@@ -0,0 +1,6 @@
+function respuesta(){
+
+var numero = document.getElementById("numero").value;
+    document.getElementById("resultado").innerHTML = "<h1 sytile='color: red'>" + "numero" + "</h1>";
+
+}
